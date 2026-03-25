6ebc366c18f2b56d3b2e64e6514937344ec9f0b2
rickyadastra <tanas.riccardo03@gmail.com>
2026-03-25 20:46:42 +0100

    feat: add x86_page_fault tool wip

--- modules/modules.js
+++ modules/modules.js
@@ -1,9 +1,15 @@
-import * as PagingTool from './x86_paging/x86-paging.js';
+import * as x86PagingTool from './x86_paging/x86-paging.js';
+import * as x86PageFaultTool from './x86_page_fault/x86-page-fault.js';
 
 export const toolsRegistry = [
     {
         id: 'x86-paging',
         tags: ['x86', 'Paging'],
-        module: PagingTool
+        module: x86PagingTool
+    },
+    {
+        id: 'x86-page-fault',
+        tags: ['x86', 'Paging', 'Exception'],
+        module: x86PageFaultTool
     }
 ]
---
+++ modules/x86_page_fault/x86-page-fault.js
@@ -0,0 +1,5 @@
+export const template = './modules/x86_page_fault/template.html';
+
+export function init() {
+    console.log('x86 page fault loaded');
+}
