b52ef4a2529eabe9f93739785f21daeb2ee66c85
rickyadastra <tanas.riccardo03@gmail.com>
2026-03-26 01:04:45 +0100

    style: use text color for tags

--- src/modules/tags.js
+++ src/modules/tags.js
@@ -7,11 +7,11 @@ export const tagsRegistry = {
     'paging': {
         text: 'Paging',
         icon: 'memory-stick',
-        classes: 'bg-blue-700 dark:bg-blue-300'
+        classes: 'bg-blue-700 dark:bg-blue-300 dark:text-blue-950'
     },
     'exception': {
         text: 'Exception',
         icon: 'bomb',
-        classes: 'bg-red-700 dark:bg-red-300'
+        classes: 'bg-red-700 dark:bg-red-300 dark:text-red-950'
     }
 };
